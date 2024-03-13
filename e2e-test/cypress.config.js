37dff59749bb6751b24a8d199aef9d0045abc600
Fidelis Wairimu <fidelis.wairimu@acquiretek.com>
2024-03-13 11:04:42 +0300

    update cicd

--- e2e-test/cypress.config.js
+++ e2e-test/cypress.config.js
@@ -1,10 +1,22 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  reporter: "cypress-mochawesome-reporter",
+  reporterOptions: {
+    charts: true,
+    reportPageTitle: "custom-title",
+    embeddedScreenshots: true,
+    inlineAssets: true,
+    saveAllAttempts: false,
+    log: true,
+    quest: true,
+  },
   e2e: {
     baseUrl: "http://ui-app.default.svc.cluster.local",
+    // baseUrl: "http://127.0.0.1:52731",
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      require("cypress-mochawesome-reporter/plugin")(on);
     },
   },
 });
--- e2e-test/cypress/e2e/spec.cy.js
+++ e2e-test/cypress/e2e/spec.cy.js
@@ -1,8 +1,10 @@
-describe('template spec', () => {
-  it('Student List was found', () => {
-    cy.wait(4000)
-    cy.visit('/');
-    
-    cy.get(".text-2xl").contains("Students List")
-  })
-})
+/// <reference types="cypress" />
+
+describe("template spec", () => {
+  it("Student List was found", () => {
+    // cy.wait(4000)
+    cy.visit("/");
+
+    cy.get(".text-2xl").contains("Students List");
+  });
+});
